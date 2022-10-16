56768b6655270cdfe474bada65b0e2ae17ea41a7
ZwingliCaleb <calebzwingli@gmail.com>
2022-10-16 14:22:15 -0700

    Print alphabets in in lowercase followed by a new line

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - lanzador.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char a = 'a';
+
+	for (a = 'a'; a <= 'Z'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
