2d925cbfb6b2dd13e88b4bc79ec50b521d50ef2c
ZwingliCaleb <calebzwingli@gmail.com>
2022-10-16 16:06:35 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Lanzador.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char a = '0';
+	char 1 = 'a';
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+	}
+	for (1 = 'a'; 1 <= 'f'; 1++)
+	{
+		putchar(1);
+	}
+	putchar('\n');
+	return (0);
+}
