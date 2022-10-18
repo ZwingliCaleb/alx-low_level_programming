39bdeac891ffd99ce19ea4920dececbf65d672a3
ZwingliCaleb <calebzwingli@gmail.com>
2022-10-18 11:24:04 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,12 +3,12 @@
 /**
  * main - Entry point
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
  * 
  */
 
 int main(void)
 {
-	write(1, "_putchar\n", 9);
-	return (0);
+write(1, "_putchar\n", 9);
+return (0);
 }
