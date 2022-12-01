2cfd22682396f62134222381873080f36c8c251b
ZwingliCaleb <calebzwingli@gmail.com>
2022-12-01 15:21:02 -0800

    new bug fix in insert function (9)

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,7 +10,7 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint-t *new = malloc(sizeof(listint_t));
+	listint_t *new = malloc(sizeof(listint_t));
 	listint_t *actualNodo = *head;
 	unsigned int i = 0;
 
