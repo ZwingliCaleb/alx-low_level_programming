800e3b536fe7e08bd691de7d8752c46f797fd316
ZwingliCaleb <calebzwingli@gmail.com>
2023-05-10 16:38:49 -0700

    fresh changes to header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -35,7 +35,14 @@ typedef struct skiplist_s
 } skiplist_t;
 
 int linear_search(int *array, size_t size, int value);
+void print_array(int *array, int left, int right);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplists_t *linear_skip(skiplist_t *list, int value);
 
 
 #endif
